9a7868a99f573deb07651c9d45af9435d05139e4
Keisuke Watanuki <watanuki@ju-zen.com>
2022-04-03 22:54:55 +0900

    Add dummy suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,54 @@
+import { Suspense } from "react";
 import "./App.css";
+
+async function fetchData(): Promise<string> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return (Math.random() * 1000).toFixed(0);
+}
+
+let data: string | undefined;
+
+function useData(): string {
+  if (data === undefined) {
+    throw fetchData().then((d) => (data = d));
+  }
+  return data;
+}
+
+function DataLoader(): JSX.Element {
+  const data = useData();
+  return (
+    <ul className="panel-list">
+      <li>{data}</li>
+      <li>Alfa</li>
+      <li>Bravo</li>
+      <li>Charlie</li>
+      <li>Delta</li>
+      <li>Echo</li>
+      <li>Foxtrot</li>
+      <li>Golf</li>
+      <li>Hotel</li>
+      <li>India</li>
+      <li>Juliett</li>
+      <li>Kilo</li>
+      <li>Lima</li>
+      <li>Mike</li>
+      <li>November</li>
+      <li>Oscar</li>
+      <li>Papa</li>
+      <li>Quebec</li>
+      <li>Romeo</li>
+      <li>Sierra</li>
+      <li>Tango</li>
+      <li>Uniform</li>
+      <li>Victor</li>
+      <li>Whiskey</li>
+      <li>X-ray</li>
+      <li>Yankee</li>
+      <li>Zulu</li>
+    </ul>
+  );
+};
 
 function App() {
   return (
@@ -9,29 +59,49 @@ function App() {
       <aside className="sidebar">
         <section className="panel">
           <h2 className="panel-headline">Baz qux</h2>
-          <p className="panel-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <p className="panel-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <p className="panel-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          <p className="panel-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p className="panel-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p className="panel-text">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className="panel-text">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+          <p className="panel-text">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
         </section>
       </aside>
       <main className="main">
         <section className="panel">
           <h2 className="panel-headline">quux corge</h2>
-          <p className="panel-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <p className="panel-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <p className="panel-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          <p className="panel-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p className="panel-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p className="panel-text">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className="panel-text">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+          <p className="panel-text">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
         </section>
         <section className="panel">
           <h2 className="panel-headline">grault garply</h2>
-          <ul className="panel-list">
-            <li>waldo</li>
-            <li>fred</li>
-            <li>plugh</li>
-            <li>wyzzy</li>
-            <li>thud</li>
-          </ul>
+          <Suspense fallback={<p className="panel-text">Loading...</p>}>
+            <DataLoader />
+          </Suspense>
         </section>
       </main>
     </div>
