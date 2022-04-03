dac0653e4892618346abe6f867c5cec0906fe04a
Keisuke Watanuki <watanuki@ju-zen.com>
2022-04-03 22:42:39 +0900

    Add style to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1 className="title">Foo bar</h1>
       </header>
+      <aside className="sidebar">
+        <section className="panel">
+          <h2 className="panel-headline">Baz qux</h2>
+          <p className="panel-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="panel-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p className="panel-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <p className="panel-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
+      </aside>
+      <main className="main">
+        <section className="panel">
+          <h2 className="panel-headline">quux corge</h2>
+          <p className="panel-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className="panel-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p className="panel-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <p className="panel-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
+        <section className="panel">
+          <h2 className="panel-headline">grault garply</h2>
+          <ul className="panel-list">
+            <li>waldo</li>
+            <li>fred</li>
+            <li>plugh</li>
+            <li>wyzzy</li>
+            <li>thud</li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
